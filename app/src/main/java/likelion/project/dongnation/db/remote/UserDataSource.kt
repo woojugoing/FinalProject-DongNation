fb11dc6fefa8f977e372d3bca674d226ff9ea4e6
Jbrochan <hyeongchanj@gmail.com>
2023-09-07 00:00:34 +0900

    Feat : Data Layer 유저 데이터 저장 기능 구현 #4

--- app/src/main/java/likelion/project/dongnation/db/remote/UserDataSource.kt
+++ app/src/main/java/likelion/project/dongnation/db/remote/UserDataSource.kt
@@ -2,7 +2,9 @@ package likelion.project.dongnation.db.remote
 
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 import likelion.project.dongnation.model.User
 
 class UserDataSource {
@@ -11,5 +13,9 @@ class UserDataSource {
     suspend fun getAllUsers(): MutableList<User> {
         val querySnapshot = db.collection("users").get().await()
         return querySnapshot.toObjects(User::class.java)
+    }
+
+    suspend fun saveUser(user: User) = withContext(Dispatchers.IO){
+        db.collection("users").add(user)
     }
 }
--- app/src/main/java/likelion/project/dongnation/repository/UserRepository.kt
+++ app/src/main/java/likelion/project/dongnation/repository/UserRepository.kt
@@ -1,9 +1,12 @@
 package likelion.project.dongnation.repository
 
 import likelion.project.dongnation.db.remote.UserDataSource
+import likelion.project.dongnation.model.User
 
 class UserRepository {
     private val userDataSource = UserDataSource()
 
     suspend fun getAllUsers() = userDataSource.getAllUsers()
+
+    suspend fun saveUser(user: User) = userDataSource.saveUser(user)
 }
