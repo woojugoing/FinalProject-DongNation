b183be0aa948ca1d756a63e0dbe223c5a8e878fc
Wise-99 <yerenkim@gmail.com>
2023-09-07 14:34:30 +0900

    Feat : ViewPager Adapter 및 tabLayout과 연결 #17

---
+++ app/src/main/java/likelion/project/dongnation/ui/donate/DonateInfoFragment.kt
@@ -0,0 +1,61 @@
+package likelion.project.dongnation.ui.donate
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+import likelion.project.dongnation.R
+import likelion.project.dongnation.databinding.FragmentDonateInfoBinding
+import likelion.project.dongnation.ui.main.MainActivity
+
+class DonateInfoFragment : Fragment() {
+
+    lateinit var fragmentDonateInfoBinding: FragmentDonateInfoBinding
+    lateinit var mainActivity: MainActivity
+
+    val imgList = ArrayList<String>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        fragmentDonateInfoBinding = FragmentDonateInfoBinding.inflate(inflater)
+        mainActivity = activity as MainActivity
+
+        fragmentDonateInfoBinding.run {
+            imgList.add(R.drawable.ic_launcher_logo_foreground.toString())
+            imgList.add(R.drawable.ic_launcher_logo_foreground.toString())
+            imgList.add(R.drawable.ic_launcher_logo_foreground.toString())
+
+            viewpager2DonateInfoThumbnail.adapter = DonateInfoFragmentStateAdapter(mainActivity)
+
+            setupTabLayoutMediator()
+        }
+
+        return fragmentDonateInfoBinding.root
+    }
+
+    inner class DonateInfoFragmentStateAdapter(fragmentActivity: FragmentActivity): FragmentStateAdapter(fragmentActivity) {
+        // 보여줄 페이지 수
+        override fun getItemCount(): Int = minOf(imgList.size, 3)
+
+        override fun createFragment(position: Int): Fragment {
+            return when (position) {
+                0 -> DonateInfoViewPagerFragment(imgList[position])
+                1 -> DonateInfoViewPagerFragment(imgList[position])
+                else -> DonateInfoViewPagerFragment(imgList[position])
+            }
+        }
+    }
+
+    fun setupTabLayoutMediator(){
+        TabLayoutMediator(
+            fragmentDonateInfoBinding.tabLayoutDonateInfoDot,
+            fragmentDonateInfoBinding.viewpager2DonateInfoThumbnail
+        ) {tab, position -> }.attach()
+    }
+}
