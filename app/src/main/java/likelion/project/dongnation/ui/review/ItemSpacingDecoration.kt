f6fcea652ba7e248c11dad9f7c3c04c3367c82eb
Wise-99 <yerenkim@gmail.com>
2023-09-09 20:14:30 +0900

    Feat : item 간격 조절을 위한 ItemDecoration class 추가 #17

---
+++ app/src/main/java/likelion/project/dongnation/ui/review/ItemSpacingDecoration.kt
@@ -0,0 +1,14 @@
+package likelion.project.dongnation.ui.review
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemSpacingDecoration(private val spacing: Int) : RecyclerView.ItemDecoration() {
+
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+        super.getItemOffsets(outRect, view, parent, state)
+
+        outRect.bottom = spacing
+    }
+}
