a82e24dfadf0d7bcb6573f54ada8de843c5a603e
juseong <wjdwntjd55@naver.com>
2023-09-09 22:09:43 +0900

    Feat : 댓글 보여주는 UI 구현 #43

--- app/src/main/java/likelion/project/dongnation/ui/board/BoardContentsFragment.kt
+++ app/src/main/java/likelion/project/dongnation/ui/board/BoardContentsFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import likelion.project.dongnation.R
 import likelion.project.dongnation.databinding.FragmentBoardContentsBinding
+import likelion.project.dongnation.databinding.ItemBoardContentsCommentBinding
 import likelion.project.dongnation.ui.main.MainActivity
 
 class BoardContentsFragment : Fragment() {
@@ -38,6 +43,11 @@ class BoardContentsFragment : Fragment() {
                 springDotsIndicatorBoardContents.setViewPager2(viewPagerBoardContents)    // indicator 설정
             }
 
+            recyclerViewBoardContents.run {
+                adapter = BoardContentsAdapter()
+                layoutManager = LinearLayoutManager(context)
+            }
+
         }
 
         return fragmentBoardContentsBinding.root
@@ -46,6 +56,49 @@ class BoardContentsFragment : Fragment() {
     // 뷰 페이저에 들어갈 아이템
     private fun getImageList(): ArrayList<String> {
         return arrayListOf<String>(R.drawable.ic_back_24dp.toString(), R.drawable.ic_launcher_logo_foreground.toString(), R.drawable.ic_launcher_logo_foreground.toString())
+    }
+
+    inner class BoardContentsAdapter : RecyclerView.Adapter<BoardContentsAdapter.BoardContentsHolder>() {
+
+        inner class BoardContentsHolder(binding: ItemBoardContentsCommentBinding) : RecyclerView.ViewHolder(binding.root){
+            val imageViewCommentItemProfile : ImageView
+            val textViewCommentItemWriter : TextView
+            val imageViewCommentItemEdit : ImageView
+            val imageViewCommentItemDelete : ImageView
+            val textViewCommentItemContents : TextView
+            val textViewCommentItemDate : TextView
+
+            init {
+                imageViewCommentItemProfile = binding.imageViewCommentItemProfile
+                textViewCommentItemWriter = binding.textViewCommentItemWriter
+                imageViewCommentItemEdit = binding.imageViewCommentItemEdit
+                imageViewCommentItemDelete = binding.imageViewCommentItemDelete
+                textViewCommentItemContents = binding.textViewCommentItemContents
+                textViewCommentItemDate = binding.textViewCommentItemDate
+            }
+
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BoardContentsHolder {
+            val binding = ItemBoardContentsCommentBinding.inflate(layoutInflater)
+            val boardContentsHolder = BoardContentsHolder(binding)
+
+            binding.root.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+
+            return boardContentsHolder
+        }
+
+        override fun getItemCount(): Int {
+            return 10
+        }
+
+        override fun onBindViewHolder(holder: BoardContentsHolder, position: Int) {
+
+        }
+
     }
 
 }
