71906a082692b4df7bb1e5a7c340adae98a5f66e
Jbrochan <hyeongchanj@gmail.com>
2023-09-09 23:14:15 +0900

    Feat : 채팅 화면 생성 및 Toolbar 기본 구현 #57
    - 채팅 화면 Fragment 생성 및 Binding 적용
    - 채팅 회면 Toolbar Theme 생성 및 적용

---
+++ app/src/main/java/likelion/project/dongnation/ui/chatting/ChattingFragment.kt
@@ -0,0 +1,34 @@
+package likelion.project.dongnation.ui.chatting
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import likelion.project.dongnation.R
+import likelion.project.dongnation.databinding.FragmentChattingBinding
+import likelion.project.dongnation.ui.main.MainActivity
+
+class ChattingFragment : Fragment() {
+
+    private lateinit var fragmentChattingBinding: FragmentChattingBinding
+    private lateinit var mainActivity: MainActivity
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        fragmentChattingBinding = FragmentChattingBinding.inflate(inflater)
+        mainActivity = activity as MainActivity
+
+        return fragmentChattingBinding.root
+    }
+
+    private fun setToolbar(){
+        fragmentChattingBinding.apply {
+            toolbarChatting.apply{
+                this.setNavigationIcon(R.drawable.ic_back_24dp)
+            }
+        }
+    }
+}
