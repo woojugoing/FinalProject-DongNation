4dec08bd39884b548d885c625b41f30ea7663019
Jbrochan <hyeongchanj@gmail.com>
2023-09-13 16:19:31 +0900

    Feat : ChattingRoom Model 멤버 변경 #57
    - 채팅 메시지를 Collection 형태가 아닌 ArrayList 형태로 저장

--- app/src/main/java/likelion/project/dongnation/model/ChattingRoom.kt
+++ app/src/main/java/likelion/project/dongnation/model/ChattingRoom.kt
@@ -1,9 +1,8 @@
 package likelion.project.dongnation.model
 
-import com.google.firebase.firestore.CollectionReference
 
 data class ChattingRoom (
     val chattingRoomUserId1: String = "",
     val chattingRoomUserId2: String = "",
-    val chattingRoomMessagesRef: CollectionReference? = null
+    val chattingRoomMessages: ArrayList<Message> = ArrayList()
 )
