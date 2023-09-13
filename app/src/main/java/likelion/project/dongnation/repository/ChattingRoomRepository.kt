bb84c1f9337f8ddf370ca5ce26ed63b991f764ad
Jbrochan <hyeongchanj@gmail.com>
2023-09-13 16:47:55 +0900

    Feat : 채팅방 Repository 채팅방 반환 및 메시지 리스트 반환 메서드 구현 #57

--- app/src/main/java/likelion/project/dongnation/repository/ChattingRoomRepository.kt
+++ app/src/main/java/likelion/project/dongnation/repository/ChattingRoomRepository.kt
@@ -11,5 +11,9 @@ class ChattingRoomRepository {
 
     suspend fun getChattingRooms(user: User) = chattingRoomDataSource.getChattingRooms(user)
 
+    suspend fun getChattingRoom(user1: User, user2: User) = chattingRoomDataSource.getChattingRoom(user1, user2)
+
     suspend fun addChattingRoom(chattingRoom: ChattingRoom) = chattingRoomDataSource.addChattingRoom(chattingRoom)
+
+    suspend fun getMessages(user1: User, user2: User) = chattingRoomDataSource.getMessages(user1, user2)
 }
