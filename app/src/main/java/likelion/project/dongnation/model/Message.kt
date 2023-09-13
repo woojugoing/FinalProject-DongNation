0c509d1fe0056b388fe2fd1364ff7daae8ab3600
Jbrochan <hyeongchanj@gmail.com>
2023-09-13 16:16:49 +0900

    Feat : Message Model 구성 요소 변경 #57
    - 인덱스 멤버 변수 삭제

--- app/src/main/java/likelion/project/dongnation/model/Message.kt
+++ app/src/main/java/likelion/project/dongnation/model/Message.kt
@@ -1,7 +1,6 @@
 package likelion.project.dongnation.model
 
 data class Message (
-    val messageIdx: Int = 0,
     val messageUserId: String = "",
     val messageContent: String = "",
     val messageDate: String = ""
