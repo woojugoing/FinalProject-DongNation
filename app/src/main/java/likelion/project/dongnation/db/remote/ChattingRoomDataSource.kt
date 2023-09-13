1b687841490741758cfc656b2eff725be67d9579
Jbrochan <hyeongchanj@gmail.com>
2023-09-13 16:43:24 +0900

    Feat : 채팅방 DataSource 특정 채팅방 탐색 메서드 구현 #57

--- app/src/main/java/likelion/project/dongnation/db/remote/ChattingRoomDataSource.kt
+++ app/src/main/java/likelion/project/dongnation/db/remote/ChattingRoomDataSource.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 import likelion.project.dongnation.model.ChattingRoom
+import likelion.project.dongnation.model.Message
 import likelion.project.dongnation.model.User
 
 class ChattingRoomDataSource {
@@ -33,10 +34,31 @@ class ChattingRoomDataSource {
         return querySnapshot.toObjects(ChattingRoom::class.java)
     }
 
+    // 해당 유저가 소속된 모든 채팅방
     suspend fun getChattingRooms(user: User): List<ChattingRoom> {
         val chattingRoomList1 = getChattingRoomsAsUser1(user)
         val chattingRoomList2 = getChattingRoomsAsUser2(user)
         return chattingRoomList1 + chattingRoomList2
+    }
+
+    // 두 명의 유저가 소속된 채팅방
+    suspend fun getChattingRoom(user1: User, user2: User): ChattingRoom {
+        val chattingRoomList1 = getChattingRooms(user1)
+        var chattingRoomResult = ChattingRoom()
+
+        for(chattingRoom in chattingRoomList1){
+            if(chattingRoom.chattingRoomUserId1 == user1.userId
+                && chattingRoom.chattingRoomUserId2 == user2.userId){
+                chattingRoomResult = chattingRoom
+                break
+            } else if(chattingRoom.chattingRoomUserId2 == user1.userId
+                && chattingRoom.chattingRoomUserId1 == user2.userId){
+                chattingRoomResult = chattingRoom
+                break
+            }
+        }
+
+        return chattingRoomResult
     }
 
     suspend fun addChattingRoom(chattingRoom: ChattingRoom) = withContext(Dispatchers.IO){
